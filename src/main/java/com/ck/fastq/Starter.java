bb84c6700d69b09dcfc58a12d248bbb7afdd0801
s4553711 <s4553711@gmail.com>
2018-05-03 14:22:06 +0800

    First release

---
+++ src/main/java/com/ck/fastq/Starter.java
@@ -0,0 +1,10 @@
+package com.ck.fastq;
+
+/**
+ * Created by s4553711 on 2018/5/3.
+ */
+public class Starter {
+    public static void main(String args[]) {
+        System.out.println("Start");
+    }
+}
